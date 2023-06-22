a01fa0780244bda754f3946fac77f142fb44ab64
3IMAD69 <dhaimy.imad@gmail.com>
2023-06-22 18:43:11 +0100

    Added create next app and restructred folder server / client

---
+++ client/tiktok-ytshorts-nextapp/src/app/test/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function test() {
+  return (
+    <div>test</div>
+  )
+}
+
---
+++ server/test.ts
@@ -0,0 +1,8 @@
+import { TiktokDL } from "@tobyg74/tiktok-api-dl";
+
+
+
+
+TiktokDL("https://www.tiktok.com/@vascolevrai/video/7240567363794504987").then(async (result) => {
+    console.log(result)
+})
