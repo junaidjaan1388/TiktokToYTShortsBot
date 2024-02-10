a52168d86ddec183bc6b4f91af414a2f2c9557aa
3IMAD <dhaimy.imad@gmail.com>
2024-02-10 14:49:50 +0100

    insta beta

---
+++ server/src/instagram_types.ts
@@ -0,0 +1,273 @@
+export interface igTypes {
+    data: Data
+    extensions: Extensions
+    status: string
+  }
+  
+  export interface Data {
+    shortcode_media: ShortcodeMedia
+  }
+  
+  export interface ShortcodeMedia {
+    __typename: string
+    id: string
+    shortcode: string
+    dimensions: Dimensions
+    gating_info: any
+    fact_check_overall_rating: any
+    fact_check_information: any
+    sensitivity_friction_info: any
+    sharing_friction_info: SharingFrictionInfo
+    media_overlay_info: any
+    media_preview: string
+    display_url: string
+    display_resources: DisplayResource[]
+    accessibility_caption: any
+    dash_info: DashInfo
+    has_audio: boolean
+    video_url: string
+    video_view_count: number
+    video_play_count: number
+    is_video: boolean
+    tracking_token: string
+    upcoming_event: any
+    edge_media_to_tagged_user: EdgeMediaToTaggedUser
+    edge_media_to_caption: EdgeMediaToCaption
+    can_see_insights_as_brand: boolean
+    caption_is_edited: boolean
+    has_ranked_comments: boolean
+    like_and_view_counts_disabled: boolean
+    edge_media_to_comment: EdgeMediaToComment
+    comments_disabled: boolean
+    commenting_disabled_for_viewer: boolean
+    taken_at_timestamp: number
+    edge_media_preview_like: EdgeMediaPreviewLike
+    edge_media_to_sponsor_user: EdgeMediaToSponsorUser
+    is_affiliate: boolean
+    is_paid_partnership: boolean
+    location: any
+    nft_asset_info: any
+    viewer_has_liked: boolean
+    viewer_has_saved: boolean
+    viewer_has_saved_to_collection: boolean
+    viewer_in_photo_of_you: boolean
+    viewer_can_reshare: boolean
+    owner: Owner
+    is_ad: boolean
+    edge_web_media_to_related_media: EdgeWebMediaToRelatedMedia
+    coauthor_producers: any[]
+    pinned_for_users: any[]
+    encoding_status: any
+    is_published: boolean
+    product_type: string
+    title: string
+    video_duration: number
+    thumbnail_src: string
+    clips_music_attribution_info: ClipsMusicAttributionInfo
+    edge_related_profiles: EdgeRelatedProfiles
+  }
+  
+  export interface Dimensions {
+    height: number
+    width: number
+  }
+  
+  export interface SharingFrictionInfo {
+    should_have_sharing_friction: boolean
+    bloks_app_url: any
+  }
+  
+  export interface DisplayResource {
+    src: string
+    config_width: number
+    config_height: number
+  }
+  
+  export interface DashInfo {
+    is_dash_eligible: boolean
+    video_dash_manifest: any
+    number_of_qualities: number
+  }
+  
+  export interface EdgeMediaToTaggedUser {
+    edges: any[]
+  }
+  
+  export interface EdgeMediaToCaption {
+    edges: Edge[]
+  }
+  
+  export interface Edge {
+    node: Node
+  }
+  
+  export interface Node {
+    created_at: string
+    text: string
+  }
+  
+  export interface EdgeMediaToComment {
+    count: number
+    page_info: PageInfo
+    edges: any[]
+  }
+  
+  export interface PageInfo {
+    has_next_page: boolean
+    end_cursor: string
+  }
+  
+  export interface EdgeMediaPreviewLike {
+    count: number
+    edges: any[]
+  }
+  
+  export interface EdgeMediaToSponsorUser {
+    edges: any[]
+  }
+  
+  export interface Owner {
+    id: string
+    is_verified: boolean
+    profile_pic_url: string
+    username: string
+    blocked_by_viewer: boolean
+    restricted_by_viewer: any
+    followed_by_viewer: boolean
+    full_name: string
+    has_blocked_viewer: boolean
+    is_embeds_disabled: boolean
+    is_private: boolean
+    is_unpublished: boolean
+    requested_by_viewer: boolean
+    pass_tiering_recommendation: boolean
+    edge_owner_to_timeline_media: EdgeOwnerToTimelineMedia
+    edge_followed_by: EdgeFollowedBy
+  }
+  
+  export interface EdgeOwnerToTimelineMedia {
+    count: number
+  }
+  
+  export interface EdgeFollowedBy {
+    count: number
+  }
+  
+  export interface EdgeWebMediaToRelatedMedia {
+    edges: any[]
+  }
+  
+  export interface ClipsMusicAttributionInfo {
+    artist_name: string
+    song_name: string
+    uses_original_audio: boolean
+    should_mute_audio: boolean
+    should_mute_audio_reason: string
+    audio_id: string
+  }
+  
+  export interface EdgeRelatedProfiles {
+    edges: Edge2[]
+  }
+  
+  export interface Edge2 {
+    node: Node2
+  }
+  
+  export interface Node2 {
+    id: string
+    full_name: string
+    is_private: boolean
+    is_verified: boolean
+    profile_pic_url: string
+    username: string
+    edge_followed_by: EdgeFollowedBy2
+    edge_owner_to_timeline_media: EdgeOwnerToTimelineMedia2
+  }
+  
+  export interface EdgeFollowedBy2 {
+    count: number
+  }
+  
+  export interface EdgeOwnerToTimelineMedia2 {
+    count: number
+    edges: Edge3[]
+  }
+  
+  export interface Edge3 {
+    node: Node3
+  }
+  
+  export interface Node3 {
+    __typename: string
+    id: string
+    shortcode: string
+    edge_media_preview_like: EdgeMediaPreviewLike2
+    edge_media_preview_comment: EdgeMediaPreviewComment
+    thumbnail_src: string
+    owner: Owner2
+    gating_info: any
+    sharing_friction_info: SharingFrictionInfo2
+    media_overlay_info?: MediaOverlayInfo
+    is_video: boolean
+    accessibility_caption?: string
+  }
+  
+  export interface EdgeMediaPreviewLike2 {
+    count: number
+  }
+  
+  export interface EdgeMediaPreviewComment {
+    count: number
+  }
+  
+  export interface Owner2 {
+    id: string
+    username: string
+  }
+  
+  export interface SharingFrictionInfo2 {
+    should_have_sharing_friction: boolean
+    bloks_app_url?: string
+  }
+  
+  export interface MediaOverlayInfo {
+    banner: any
+    buttons: Button[]
+    description: string
+    icon: Icon
+    misinformation_type: any
+    overlay_layout: number
+    overlay_type: string
+    title: string
+    bloks_data: string
+  }
+  
+  export interface Button {
+    button_type: number
+    text: string
+    secondary_text: any
+    action: number
+    action_url?: string
+    icon: any
+    text_color: TextColor
+    is_text_centered: any
+    has_chevron: any
+  }
+  
+  export interface TextColor {
+    light: string
+    dark: string
+  }
+  
+  export interface Icon {
+    icon_glyph: number
+    icon_type: number
+    name: string
+    src: any
+  }
+  
+  export interface Extensions {
+    is_final: boolean
+  }
+  
--- server/src/instragram.ts
+++ server/src/instragram.ts
@@ -1,25 +1,63 @@
 import Downloader from "nodejs-file-downloader"
 import axios from 'axios'
 import { UploadShorts } from './UploadShorts';
-
+import {igTypes} from './instagram_types';
 //ba9i makhdmaach dyal insta
-async function GetReelDetails(Reelurl:string,ShortTitle:string) {
-    
-    const {data} = await axios.post('https://savein.io/api/fetchv2',{
-        url : Reelurl
-    })
 
-      if(data.success){
-        DownloadReel(data.data.urls[0],ShortTitle)
-      }else{
-        console.log('ERROR')
-      }
-     
-       
+function get_hashtags(text: string): string[] {
+  const regex = /#[^\s#]+/g;
+  const hashtags = text.match(regex) || [];
+  return hashtags;
+}
+
+export async function GetReelDetails(Reelurl:string,ShortTitle:string,Dwload_Flag : boolean) {
+    
+    const regex = /\/(?:p|reel|reels)\/([A-Za-z0-9_-]+)\/?(\?.*)?/;
+    const match = Reelurl.match(regex);
+    /*
+    tiktokLink: string;
+    title?:string,
+    description?:string,
+    dynamic_cover?:string,
+    logo: boolean;
+    filter: boolean;
+    duration:number;
+    isWorking:boolean
+    */
+    if (match && match[1]) {
+        const reelId = match[1];
+        console.log("Reel ID:", reelId);
+        console.log(`https://www.instagram.com/graphql/query/?query_hash=b3055c01b4b222b8a47dc12b090e4e64&variables={"shortcode":"${reelId}"}`)
+        const {data} = await axios.get<igTypes>(`https://www.instagram.com/graphql/query/?query_hash=b3055c01b4b222b8a47dc12b090e4e64&variables={"shortcode":"${reelId}"}`)
+        
+        // console.log(Reelurl);
+        // console.log(data.data.shortcode_media.edge_media_to_caption.edges[0].node.text)
+        // console.log(get_hashtags(data.data.shortcode_media.edge_media_to_caption.edges[0].node.text).join(' '))
+        // console.log(data.data.shortcode_media.video_duration.toFixed(2));
+        // console.log(data.data.shortcode_media.display_url)
+        if(Dwload_Flag){
+          DownloadReel(data.data.shortcode_media.video_url)
+          return ;
+        }else{
+          return 
+          {
+              reelUrl : Reelurl,
+              title : data.data.shortcode_media.edge_media_to_caption.edges[0].node.text,
+              description : get_hashtags(data.data.shortcode_media.edge_media_to_caption.edges[0].node.text).join(' ') ,
+              dynamic_cover  : data.data.shortcode_media.display_url,
+              duration : data.data.shortcode_media.video_duration.toFixed(2)
+          }
+        }
+    } else {
+        console.log("Reel ID not found in the link. => "+ Reelurl);
+        return ;
+        //delete reel from db cuz of wrong link wla gha ana  hmar ma3rftch nhandlih
+    }
+
   //  DownloadReel(data.data.urls[0],data.data.caption)
 }
 
-async function DownloadReel(url:string,desc:string) {
+async function DownloadReel(url:string) {
     const filename = 'Motivation-N'+Math.floor(Math.random() * 10000)
     const downloader = new Downloader({
         url: url,
@@ -39,4 +77,4 @@ async function DownloadReel(url:string,desc:string) {
 
 
 
-GetReelDetails("https://www.instagram.com/reels/CrYdqEIocCR","Be the 1% #DavidGoggins ")
+GetReelDetails("https://www.instagram.com/reels/Cz4NR-KIu_H/","Be the 1% #DavidGoggins ",false)
